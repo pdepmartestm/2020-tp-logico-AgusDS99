10d39f085a715e3f4be9e0c9ffe185bd6aeb6d61
lspigariol <lspigariol@gmail.com>
2020-06-16 09:20:17 -0300

    variantes1

--- tplogico.pl
+++ tplogico.pl
@@ -9,11 +9,10 @@ invitado(lucia).
 invitado(lucas).
 invitado(martina).
 invitado(javier).
-invitado(camila).
+invitado(camila).   
 
 %% Para más organización se colocó en una lista a todos los invitados. Utilizando la funcion member se puede consultar si determinada persona está dentro de la lista de invitados.
-
-listaDeInvitados(["agustin","gabriel","martin","lucia","lucas","martina","javier","camila"]).
+%listaDeInvitados(["agustin","gabriel","martin","lucia","lucas","martina","javier","camila"]).
 
 estaDisponible(agustin).
 estaDisponible(gabriel).
@@ -23,33 +22,42 @@ estaDisponible(lucas).
 estaDisponible(martina).
 estaDisponible(camila).
 
-viveEn(agustin,ciudadEvita,noEstaLloviendo).
-viveEn(gabriel,ciudadEvita,noEstaLloviendo).
-viveEn(martin,parqueChacabuco,estaLloviendo).
-viveEn(lucia,mataderos,noEstaLloviendo).
-viveEn(lucas,sanJusto,noEstaLloviendo).
-viveEn(martina,boedo,noEstaLloviendo).
-viveEn(javier,almagro,noEstaLloviendo).
-viveEn(camila,almagro,noEstaLloviendo).
+viveEn(agustin,ciudadEvita).
+viveEn(gabriel,ciudadEvita).
+viveEn(martin,parqueChacabuco).
+viveEn(lucia,mataderos).
+viveEn(lucas,sanJusto).
+viveEn(martina,boedo).
+viveEn(javier,almagro).
+viveEn(camila,almagro).
+
+clima(ciudadEvita,noEstaLloviendo).
+clima(parqueChacabuco,estaLloviendo).
+clima(mataderos,noEstaLloviendo).
+clima(sanJusto,noEstaLloviendo).
+clima(boedo,noEstaLloviendo).
+clima(almagro,noEstaLloviendo).
+
+quedaEn(ciudadEvita,laMatanza).
+quedaEn(sanJusto,laMatanza).
+quedaEn(almagro,bsas).
+quedaEn(boedo,bsas).
+quedaEn(mataderos,bsas).
+quedaEn(parqueChacabuco,bsas).
 
 %% Se toma en cuenta vivir cerca como estar dentro de CABA o La Matanza).
 
-viveCerca(agustin).
-viveCerca(gabriel).
-viveCerca(martin).
-viveCerca(lucia).
-viveCerca(lucas).
-viveCerca(martina).
-viveCerca(javier).
-viveCerca(camila).
+estaCerca(UnaCiudad,UnaCiudad).
+estaCerca(UnaCiudad,OtraCiudad):-
+    quedaEn(UnaCiudad,Zona),
+    quedaEn(OtraCiudad,Zona).
 
-leGustaElAsado(agustin,leGusta).
-leGustaElAsado(gabriel,leGusta).
-leGustaElAsado(martin,leGusta).
-leGustaElAsado(lucia,leGusta).
-leGustaElAsado(lucas,leGusta).
-leGustaElAsado(martina,leGusta).
-leGustaElAsado(javier,noLeGusta).
+leGustaElAsado(agustin).
+leGustaElAsado(gabriel).
+leGustaElAsado(martin).
+leGustaElAsado(lucia).
+leGustaElAsado(lucas).
+leGustaElAsado(martina).
 
 %% Me permite conocer si a un invitado le alcanza el dinero para el asado. En caso de no alcanzarle el resto pondrá lo que falta por él. Es por eso que no es determinante para saber
 %% si puede asistir o no. El calculo se realiza en base al dinero que tiene disponible menos el dinero que debe gastar en viaje.
@@ -61,9 +69,10 @@ leAlcanzaElDineroA(Invitado,DineroGastadoEnViaje,DineroDisponible) :-
 %% Con el predicado "puedeAsistir(Invitado,Ciudad") se puede consultar si determinada persona puede asistir al asado. También, para obtener la totalidad de personas que pueden asistir,
 %% se puede aplicar el concepto de inversibilidad para que me indique quién o quiénes hacen cumplir el predicado.
 
-puedeAsistir(Invitado,Ciudad) :-
+puedeAsistir(Invitado,CiudadAsado) :-
     invitado(Invitado),
     estaDisponible(Invitado),
-    viveEn(Invitado,Ciudad,noEstaLloviendo),
-    viveCerca(Invitado),
-    leGustaElAsado(Invitado,leGusta).
+    clima(CiudadAsado,noEstaLloviendo),
+    viveEn(Invitado,Ciudad),
+    estaCerca(Ciudad,CiudadAsado),
+    leGustaElAsado(Invitado).
